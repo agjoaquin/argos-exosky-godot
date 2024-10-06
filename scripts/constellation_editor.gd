050ac04db5ad4b318e7d135e2e40a8560298135b
agjoaquin <102189208+agjoaquin@users.noreply.github.com>
2024-10-06 12:24:51 -0300

    commited graph constelations

--- scripts/constellation_editor.gd
+++ scripts/constellation_editor.gd
@@ -1,21 +1,44 @@
 extends SubViewportContainer
 
 @onready var texture_rect: TextureRect = %TextureRect
+@onready var sub_viewport: SubViewport = $SubViewport
 
+var draw_active = false
+var line = Line2D.new()
+
+func _ready() -> void:
+	
+	texture_rect.add_child(line)
 
 func set_texture(texture : Texture):
 	texture_rect.texture = texture
 
+func _input(event: InputEvent) -> void:
+	if not draw_active:
+		return
+	if event is InputEventMouseButton:
+		match event.button_index:
+			MOUSE_BUTTON_LEFT:
+					if event.pressed:
+						print("event.position",event.position)
+						print("")
+						line.width = 2
+						var color =  Color(0,0.4,3,0.5)
+						line.default_color = color
+						if event.position.x-135>0 and event.position.y<697:
+							line.add_point(Vector2(event.position.x-135,event.position.y))
+				#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED if event.pressed else Input.MOUSE_MODE_VISIBLE)
+	
+	
 
 func _on_capture_button_pressed() -> void:
 	# Asegúrate de que hay una textura en el TextureRect
 	if texture_rect.texture:
 		# Convertir la textura a una imagen
-		var image: Image = texture_rect.texture.get_image()
+		var image: Image = sub_viewport.get_texture().get_image()
 		
 		# Asegúrate de que los datos de la imagen están disponibles
 		if image:
-			image.flip_y()  # Voltea la imagen verticalmente si es necesario
 			
 			# Guardar la imagen como un PNG
 			var file_path = "res://captures/capture.png" # Ruta donde se guardará el archivo
@@ -29,3 +52,10 @@ func _on_capture_button_pressed() -> void:
 			print("No se pudo obtener la imagen de la textura.")
 	else:
 		print("No hay textura en el TextureRect.")
+
+
+func _on_draw_button_pressed() -> void:
+	draw_active = !draw_active
+	
+	if not draw_active:
+		line.clear_points()
